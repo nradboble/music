7747e8555ccad5746f433cf46f37fb16101a8c05
nradboble <51375454+nradboble@users.noreply.github.com>
2019-06-24 10:05:29 -0600

    Add files via upload

---
+++ app/components/Itunes/itunes-controller.js
@@ -0,0 +1,31 @@
+import ItunesService from "./itunes-service.js";
+//Private
+const itunesService = new ItunesService()
+
+function drawSongs() {
+  //changes button back to GET MUSIC once songs are loaded
+  document.querySelector('#get-music-button').textContent = 'GET MUSIC'
+
+
+}
+
+
+//PUBLIC
+class ItunesController {
+  constructor() {
+    //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+  }
+
+
+  //DO NOT MODIFY THIS METHOD
+  getMusic(e) {
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    //changes the button to loading while songs load
+    document.querySelector('#get-music-button').textContent = 'LOADING....'
+    itunesService.getMusicByArtist(artist)
+  }
+}
+
+
+export default ItunesController
